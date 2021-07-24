66509123daa73346b1e89f0c92291b863ee1989d
jvillegasd <linkrsfan@gmail.com>
2021-07-24 21:06:03 +0200

    Error handling to json added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from environs import Env
 from flask_cors import CORS
-from flask import Flask, jsonify
+from flask import Flask, jsonify, json
+from werkzeug.exceptions import HTTPException
 
 from modules import user_blueprint
 
@@ -19,6 +20,18 @@ app.register_blueprint(user_blueprint, url_prefix='/users')
 @app.route('/')
 def ping():
   return jsonify({'message': 'server is up!'}), 200
+
+# Error handling to json
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+  response = e.get_response()
+  response.data = json.dumps({
+    "code": e.code,
+    "name": e.name,
+    "description": e.description,
+  })
+  response.content_type = "application/json"
+  return response
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=env('FLASK_PORT'), debug=True)
